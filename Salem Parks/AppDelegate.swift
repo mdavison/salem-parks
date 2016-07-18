ee4cd351d18dfcccf32b2a6d8583c3583b5eb6da
Morgan Davison <morgan.davison@gmail.com>
2016-07-18 09:17:01 -0700

    Design launch screen, cleanup

--- Salem Parks/AppDelegate.swift
+++ Salem Parks/AppDelegate.swift
@@ -34,13 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         let mapViewController = mapViewNavController.viewControllers[0] as! MapViewController
         mapViewController.coreDataStack = coreDataStack
         
-//        let parksListNavigationController = tabBarController.viewControllers![2] as! UINavigationController
-//        let parksListViewController = parksListNavigationController.viewControllers[0] as! ParksListViewController
-//        parksListViewController.coreDataStack = coreDataStack
-        
-//        let settings = UIUserNotificationSettings(forTypes: [.Badge], categories: nil)
-//        application.registerUserNotificationSettings(settings)
-//        application.registerForRemoteNotifications()
         let notificationSettings = UIUserNotificationSettings(forTypes: [.Sound, .Alert], categories: nil)
         application.registerUserNotificationSettings(notificationSettings)
         UIApplication.sharedApplication().cancelAllLocalNotifications()
@@ -56,46 +49,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         
         return true
     }
-    
-//    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
-//        
-//        //print("didRegisterForRemoteNotificationsWithDeviceToken called: \(deviceToken.bytes)")
-//        
-//        if userIsSignedIntoiCloud {
-//            Park.subscribeToiCloudChanges()
-//            
-//            defaultNotificationCenter.addObserverForName(
-//                CloudKitNotifications.notificationReceived,
-//                object: nil,
-//                queue: NSOperationQueue.mainQueue(),
-//                usingBlock: { notification in
-//                    if let ckQueryNotification = notification.userInfo?[CloudKitNotifications.notificationKey] as? CKQueryNotification {
-//                        self.iCloudHandleSubscriptionNotification(ckQueryNotification)
-//                    }
-//            })
-//        }
-//    }
-    
-//    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
-//        
-//        print("didFailToRegisterForRemoteNotificationsWithError error: \(error.localizedDescription)")
-//    }
-    
-//    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
-//        
-//        print("recieved remote notification with userInfo: \(userInfo)")
-//        let cloudKitQueryNotification = CKQueryNotification(fromRemoteNotificationDictionary: userInfo as! [String: NSObject])
-//        let notification = NSNotification(
-//            name: CloudKitNotifications.notificationReceived,
-//            object: self,
-//            userInfo: [CloudKitNotifications.notificationKey: cloudKitQueryNotification]
-//        )
-//        
-//        defaultNotificationCenter.postNotification(notification)
-//        
-//        completionHandler(.NewData)
-//
-//    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -123,35 +76,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     }
     
     
-//    private func iCloudHandleSubscriptionNotification(ckQueryNotification: CKQueryNotification) {
-//        print("iCloudHandleSubscriptionNotification called")
-//        
-//        //print("notification info: \(ckQueryNotification.recordID)")
-//        if let recordID = ckQueryNotification.recordID {
-//            // Get the CKRecord from icloud, which posts a notification when it's done
-//            Park.getPark(forCKRecordID: recordID, coreDataStack: coreDataStack)
-//            
-//            // Add the observer because we have to wait for icloud to come back before we can do anything with the CKRecord
-//            defaultNotificationCenter.addObserverForName(
-//                Notifications.getParkForCKRecordIDFinishedNotification,
-//                object: nil,
-//                queue: NSOperationQueue.mainQueue(),
-//                usingBlock: { notification in
-//                    if let park = notification.userInfo?["Park"] as? Park {
-//                        park.hasNewPhotos = true
-//                        self.coreDataStack.saveContext()
-//                        
-//                        // post another notification that hasNewPhotosFinishedNotification
-//                        let notification = NSNotification(name: CloudKitNotifications.hasNewPhotosFinishedNotification, object: self)
-//                        self.defaultNotificationCenter.postNotification(notification)
-//                    }
-//                }
-//            )
-//        }
-//    }
-    
     func handleRegionEvent(region: CLRegion) {
-        print("Geofence triggered!")
         if UIApplication.sharedApplication().applicationState == .Active {
             // Alert near a park
             if let viewController = window?.rootViewController {
